9e308117dec16b7a53c24e57c40cc523e8bb1526
Nhan Tran <nhantran@ksu.edu>
2016-02-26 19:47:59 -0600

    update

--- Legendre.py
+++ Legendre.py
@@ -3,14 +3,23 @@ import math
 
 
 def doublefactorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n*doublefactorial(n-2)
+    res = 1
+    while n>=2:
+        res *= n
+        n -= 2
         
-         
-#Compute the Associated Legendre polynomials of type 1 with complex z
-#|m| <= l
+    return res
+
+
+#def doublefactorial(n):
+#    if n <= 1:
+#        return 1
+#    else:
+#        return n*doublefactorial(n-2)
+        
+
+#Compute the Associated Legendre polynomials of type 1 with complex z recursively
+#l,m > 0, |m| <= l
 def P(l,m,z):    
     if(np.abs(m)>l):
         return 0
@@ -22,10 +31,10 @@ def P(l,m,z):
         return z*(2*m+1)*P(m, m, z)
     
     return (z*(2*l-1)*P(l-1, m, z) - (l+m-1)*P(l-2, m, z))/(l-m)
-        
+                    
 
-#Compute the Associated Legendre polynomials of type 1 with complex z
-#Equivalent to scipy.special.clpmn(m, n, z, type=2), but can take negative m, l
+#Compute the Associated Legendre polynomials of type 1 with complex z recursively
+#Equivalent to scipy.special.clpmn(m, n, z)*1j, but can take negative m, l
 #|m| <= l
 def LegendrePoly(l,m,z):
     if(np.abs(m)>l):
@@ -37,4 +46,23 @@ def LegendrePoly(l,m,z):
     if m<0 and l<0:
         return 0
     
-    return P(l,m,z)         
+    return P(l,m,z)
+    
+
+#Compute the Associated Legendre polynomials of type 1 with complex z non-recursively
+#Equivalent to scipy.special.clpmn(m, n, z)    
+def LP(l,m,z): 
+    p = np.zeros((m+1,l+1), dtype=complex)
+    
+    p[0,0] = 1
+    for l1 in range(m+1):
+        p[l1,l1] = ((-1)**l1)*doublefactorial(2*l1-1)*((1-z*z)**(l1/2))
+        
+    for l1 in range(m+1):
+        p[l1,l1+1] = z*(2*l1+1)*p[l1, l1]
+    
+    for l1 in range(m+1):        
+        for l2 in range(m+2,l+1):
+            p[l1,l2] = (z*(2*l2-1)*p[l1, l2-1] - (l2+l1-1)*p[l1,l2-2])/(l2-l1)
+            
+    return p
